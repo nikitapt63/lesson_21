6da5b2d6cb2ac91ac7a7bae82591f52b6d9ebd9b
nikitapt63 <nikitapt63@gmail.com>
2023-01-19 19:56:50 +0300

    Create Page model

--- blog/news_feed/models.py
+++ blog/news_feed/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class DateTimeMixin:
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+
+class Page(models.Model, DateTimeMixin):
+    title = models.CharField(max_length=150)
+    description = models.CharField(max_length=255)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_private = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.pk} - {self.title}"
+
+    class Meta:
+        verbose_name = "Page"
+        verbose_name_plural = "Pages"
