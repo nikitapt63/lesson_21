5874bb5d9082267c4e58c78a0c86513aeff50ad7
nikitapt63 <nikitapt63@gmail.com>
2023-01-19 22:03:40 +0300

    Add views

--- blog/news_feed/views.py
+++ blog/news_feed/views.py
@@ -1,3 +1,26 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, DeleteView, CreateView, UpdateView
 
-# Create your views here.
+from .models import Page, Post
+
+
+def say_hello(request):
+    return HttpResponse("Hello")
+
+
+def get_pages_list(request):
+    context = {"all_pages": Page.objects.all()}
+    return render(request, "all_pages.html", context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = "all_posts.html"
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ["name", "content", "page"]
+    template_name = "post_create.html"
+    success_url = "/"
